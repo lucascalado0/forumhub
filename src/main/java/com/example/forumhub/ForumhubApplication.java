09ca12ed6df6dbc6682777fc5dca50b8428d1864
devlucascalado <devlucascalado@gmail.com>
2025-01-15 16:39:58 -0300

    iniciando projeto e adicionando dependencias

---
+++ src/main/java/com/example/forumhub/ForumhubApplication.java
@@ -0,0 +1,19 @@
+package com.example.forumhub;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForumhubApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForumhubApplication.class, args);
+	}
+
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("alo mundor");
+	}
+}
